d47c8c8d4555fefcfebe33092a76fa637d50fa3f
IT24102231 <it24102231@my.sliit.lk>
2024-10-03 10:35:34 -0700

    IT2410223Lab7Q2A.java

---
+++ Q1/IT2410223Lab7Q2A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class IT2410223Lab7Q2A {
+    public static void main(String[]args) {
+      Scanner reader = new Scanner(System.in);
+	  int i,j;
+	  for(j = 1;j <= 4;j++) {
+	     for(i = 1;i <=5;i++) {
+		    System.out.print("$");
+		 }
+		 System.out.print("\n");
+	  }
+	}
+}
